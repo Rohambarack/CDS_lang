32e9ad0cc6f7ddff41025e63a13bfcb553836a26
Rohambarack <ucloud@j-5023040-job-0.j-5023040.ucloud-apps.svc.cluster.local>
2024-03-01 15:49:04 +0100

    session5

--- nbs_vis/vis_setup.sh
+++ nbs_vis/vis_setup.sh
@@ -3,3 +3,7 @@ sudo apt-get update
 sudo apt-get install -y python3-opencv
 pip install opencv-python
 pip install matplotlib
+pip install pandas
+pip install numpy
+pip install seaborn
+pip install scikit-learn
