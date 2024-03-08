695e554a495817582467fad25498dfe870c74dc4
Rohambarack <ucloud@j-5025451-job-0.j-5025451.ucloud-apps.svc.cluster.local>
2024-03-08 13:38:05 +0100

    coding practices increased s_6

--- nbs_vis/vis_s_06/src/test_scrip.py
+++ nbs_vis/vis_s_06/src/test_scrip.py
@@ -1,9 +1,22 @@
 import os
 os.system("pip install numpy")
+import argparse 
 import numpy as np
 
+def file_loader():
+    parser = argparse.ArgumentParser(description = "Loading and printing an array")
+
+    parser.add_argument("--input",
+                        "-i",
+                        required = True,
+                        help = "Filepath to csv for loading and printing")
+    args = parser.parse_args()
+
+    return args
+
 def main():
-    filename = os.path.join("..","in","sample-data-01.csv")
+    args = file_loader()
+    filename = os.path.join("..","in",args.input)
     data = np.loadtxt(filename, delimiter= ",")
 
     print(data)
