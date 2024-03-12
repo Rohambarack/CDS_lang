a13e2b7b5d5c0f3fede95986ad76bf7e17515cbf
Rohambarack <ucloud@j-5026457-job-0.j-5026457.ucloud-apps.svc.cluster.local>
2024-03-12 13:11:12 +0100

    start on asss_2

---
+++ assingments_vis/assignment2/ucloud_python_init.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get install -y python3-opencv
+pip install opencv-python
+pip install matplotlib
+pip install pandas
+pip install numpy
+pip install scikit-learn
+pip install tensorflow
