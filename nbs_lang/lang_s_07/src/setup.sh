ce4c7cbcf29a109c18d519e429b70b52d223c99b
Rohambarack <ucloud@j-5027815-job-0.j-5027815.ucloud-apps.svc.cluster.local>
2024-03-15 11:25:06 +0100

    lang_py srict setup with pipreqs

---
+++ nbs_lang/lang_s_07/src/setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+#createv virtual env
+python -m venv env
+#actvate env
+source ./env/bin/activate
+# pip valami with pipreqs, save exact libraries with version number
+# pip install requirements.txt
